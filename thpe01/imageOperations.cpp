f061e77ea5ea5b15562182cfc96ec10d1fc99508
Wilk, Nicholas <nicholas.wilk@mines.sdsmt.edu>
2022-10-11 22:30:53 -0600

    thpe01 - [ci skip] - finished brighten.

--- thpe01/imageOperations.cpp
+++ thpe01/imageOperations.cpp
@@ -32,38 +32,51 @@ void Grayscale(image& data)
 void Brighten( char* arg2, image& data)
 {
     int val = atoi(arg2);
+    unsigned long int newval, finalVal;
     int i, j;
     for (i = 0; i < data.cols; i++)
     {
         for (j = 0; j < data.rows; j++)
         {
-            data.redgray[i][j] = data.redgray[i][j] + val;
-            data.green[i][j] = data.green[i][j] + val;
-            data.blue[i][j] = data.blue[i][j] + val;
-            if (data.redgray[i][j] > 255 )
+            newval = data.redgray[i][j];
+            finalVal = newval + val;
+            if (finalVal > 255)
             {
-                data.redgray[i][j] = 255;
+                finalVal = 255;
+                data.redgray[i][j] = finalVal;
             }
-            if (data.redgray[i][j] < 0)
+            if (finalVal < 0)
             {
-                data.redgray[i][j] = 0;
+                finalVal = 0;
+                data.redgray[i][j] = finalVal;
             }
-            if (data.green[i][j] > 255)
+            data.redgray[i][j] = finalVal;
+            newval = data.green[i][j];
+            finalVal = newval + val;
+            if (finalVal > 255)
             {
-                data.green[i][j] = 255;
+                finalVal = 255;
+                data.green[i][j] = finalVal;
             }
-            if (data.green[i][j] < 0)
+            if (finalVal < 0)
             {
-                data.green[i][j] = 0;
+                finalVal = 0;
+                data.green[i][j] = finalVal;
             }
-            if (data.blue[i][j] > 255)
+            data.green[i][j] = finalVal;
+            newval = data.blue[i][j];
+            finalVal = newval + val;
+            if (finalVal > 255)
             {
-                data.blue[i][j] = 255;
+                finalVal = 255;
+                data.blue[i][j] = finalVal;
             }
-            if (data.blue[i][j] < 0)
+            if (finalVal < 0)
             {
-                data.blue[i][j] = 0;
-            }     
+                finalVal = 0;
+                data.blue[i][j] = finalVal;
+            }
+            data.blue[i][j] = finalVal;
         }
     }
 }
